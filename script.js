3ed2405d66a520566b9e39cbc56745d0fc5edb78
Gintstir <gint.stirbys@gmail.com>
2020-11-08 14:10:48 -0500

    added seperate .html for highscores, started styling in .css, creating hidden buttons inside index.html

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 var startButton = document.querySelector(".start-button");
 var quizArea = document.querySelector(".quiz-area");
+var 
 var currentQuestion = 0;
+
+
+
+
+
+
 var quizQuestions = [
     {
         question: "In JavaScript there are two ways to create a function they are:",
         choices: [
-            "1. function expression and function declaration",
-            "2. function extrusion and function induction",
-            "3. function exhaltation and function demarcation",
-            "4. function examination and function extrapolation"
+            "1. Function expression and function declaration",
+            "2. Function extrusion and function induction",
+            "3. Function exhaltation and function demarcation",
+            "4. Function examination and function extrapolation"
         ],
-        correctAnswer: "1. function expression and function declaration"
+        correct: "1. function expression and function declaration"
     },
     {
         question: "Commentary in JavaScript can be marked(but not displayed in the browser) with which of the following notations?",
@@ -53,7 +60,7 @@ var quizQuestions = [
         correct: "3. Number"
     },
     {
-        question: "Which of these is the strict ineqality operator",
+        question: "Which of these is the strict ineqality operator?",
         choices: [
             "1. >=",
             "2. ===",
@@ -62,10 +69,8 @@ var quizQuestions = [
         ],
         correct: "4. !=="
     }
-]
-
-
-
+];
+ 
 
 startButton.addEventListener("click", function(){
     quizArea.innerHTML = "<h1>" + quizQuestions[currentQuestion].question + "</h1>" +
@@ -77,8 +82,8 @@ startButton.addEventListener("click", function(){
 });
 
 // bc ansr choices are created with js we must first listen to a static element and then specify in the addEvenListener
-quizArea.addEventListener('click',  function(e) {
-    if(e.target.tagName.toLowerCase() === 'p') {
+quizArea.addEventListener("click",  function(e) {
+    if(e.target.tagName.toLowerCase() === "p") {
         currentQuestion++;
         quizArea.innerHTML = "<h1>" + quizQuestions[currentQuestion].question + "</h1>" +
         "<p class='ansrChoice'>" + quizQuestions[currentQuestion].choices[0] + "</p>" +
@@ -86,7 +91,12 @@ quizArea.addEventListener('click',  function(e) {
         "<p class='ansrChoice'>" + quizQuestions[currentQuestion].choices[2] + "</p>" +
         "<p class='ansrChoice'>" + quizQuestions[currentQuestion].choices[3] + "</p>" ;
     }
+    
 })
+
+
+
+//-----------------------------------------------------------------------------------------------------------------------------
 //Timer function
 function startTimer() {
     var counter =80;
